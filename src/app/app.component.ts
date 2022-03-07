3bd75329a22cb0fc1665db3228d1a8357bbd6d61
Raf <raffele.corgiat@gmail.com>
2022-03-07 12:07:45 +0100

    prime modifiche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+  // decoratore
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title: string = 'Tour of Heroes';
 }
