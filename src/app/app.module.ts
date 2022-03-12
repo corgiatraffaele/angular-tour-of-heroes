10b1ff2dd670c20725c3bee4cba95231b45bd430
Raf <raffele.corgiat@gmail.com>
2022-03-12 12:51:24 +0100

    add lista heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; 
+import { FormsModule } from '@angular/forms'; //importato per ngModel
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, //importato per ngModel
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from 'src/app/mock-data/mock-heroes';
+import { Hero } from 'src/app/models/hero'; //importa il file hero.ts(il .ts viene omesso)
 
-@Component({
+@Component({   //quando trovi l'app-heroes buttaci quel html e scss//
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: string='Windstorm';
+
+  heroes: Hero[] = HEROES;
+
+
+
+
+
+    //values: number[] = [2,6,8,1570]
+
+
 
   constructor() { }
 
